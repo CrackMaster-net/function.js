b36e1e34c35b7bfb48ae620499a796966309bf49
CrackMaster.net <marcgeminiano43@gmail.com>
2025-01-11 02:07:45 +0800

    Create quiz.js
    Output

---
+++ quiz.js
@@ -0,0 +1,79 @@
+//Question Number 1
+function myFirstQuestionCorret () {
+    document.getElementById("question1").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function myFirstQuestionWrong () {
+    document.getElementById("question1").innerHTML = "Wrong Answer";
+}
+
+//Question Number 2
+function mySecondQuestionCorret () {
+    document.getElementById("question2").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function mySecondQuestionWrong () {
+    document.getElementById("question2").innerHTML = "Wrong Answer";
+}
+
+//Question Number 3
+function myThirdQuestionCorrect () {
+    document.getElementById("question3").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function myThirdQuestionWrong () {
+    document.getElementById("question3").innerHTML = "Wrong Answer";
+}
+
+//Question Number 4
+function myFourthQuestionCorrect () {
+    document.getElementById("question4").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function myFourthQuestionWrong () {
+    document.getElementById("question4").innerHTML = "Wrong Answer";    
+}
+
+//Question Number 5
+function myFifthQuestionCorrect () {
+    document.getElementById("question5").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function myFifthQuestionWrong () {
+    document.getElementById("question5").innerHTML = "Wrong Answer";
+}
+
+//Question Number 6
+function mySixthQuestionCorrect () {
+    document.getElementById("question6").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function mySixthQuestionWrong () {
+    document.getElementById("question6").innerHTML = "Wrong Answer";
+}
+
+//Question Number 7
+function mySeventhQuestionCorrect () {
+    document.getElementById("question7").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function mySeventhQuestionWrong () {
+    document.getElementById("question7").innerHTML = "Wrong Answer";
+}
+
+//Question Number 8
+function myEighthQuestionCorrect () {
+    document.getElementById("question8").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function myEighthQuestionWrong () {
+    document.getElementById("question8").innerHTML = "Wrong Answer";
+}
+
+//Question Number 9
+function myNinthQuestionCorrect () {
+    document.getElementById("question9").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function myNinthQuestionWrong () {
+    document.getElementById("question9").innerHTML = "Wrong Answer";
+}
+
+//Question Number 10
+function myTenthQuestionCorrect () {
+    document.getElementById("question10").innerHTML = "<p style='color: green;'>Correct</p>";
+}
+function myTenthQuestionWrong () {
+    document.getElementById("question10").innerHTML = "Wrong Answer";
+}
