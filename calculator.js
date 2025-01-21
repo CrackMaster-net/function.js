ffd5b2cadc570922f991ac27d89db043d080de43
One Thousand Minus Seven? <marcgeminiano43@gmail.com>
2025-01-21 23:59:29 +0800

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,31 @@
+function addition() { // start creating a function
+    var x, y, z; // declare variables
+    var x = Number(document.getElementById("number1").value); // get the value of the first input field
+    var y = Number(document.getElementById("number2").value); // get the value of the first input field
+    var z = x + y; // add the two number together
+    document.getElementById("result").innerHTML = z; // display the result
+}
+
+function subtraction() {
+    var x, y, z;
+    var x = Number(document.getElementById("number1").value);
+    var y = Number(document.getElementById("number2").value);
+    var z = x - y;
+    document.getElementById("result").innerHTML = z;
+}
+
+function multiplication() {
+    var x, y, z;
+    var x = Number(document.getElementById("number1").value);
+    var y = Number(document.getElementById("number2").value);
+    var z = x * y;
+    document.getElementById("result").innerHTML = z;
+}
+
+function division() {
+    var x, y, z;
+    var x = Number(document.getElementById("number1").value);
+    var y = Number(document.getElementById("number2").value);
+    var z = x / y;
+    document.getElementById("result").innerHTML = z;
+}
