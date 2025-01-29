239509ded268225940bd3298f75d93c0ceacf030
One Thousand Minus Seven? <marcgeminiano43@gmail.com>
2025-01-29 20:23:57 +0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -77,3 +77,5 @@ function myTenthQuestionCorrect () {
 function myTenthQuestionWrong () {
     document.getElementById("question10").innerHTML = "Wrong Answer";
 }
+
+//This JavaScript has only two "function"
